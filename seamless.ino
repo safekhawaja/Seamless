c6b51cc7a15a1b155814298f432e51404bffad80
Saif <68385788+safekhawaja@users.noreply.github.com>
2020-12-29 09:02:28 -0500

    update w outputs

--- seamless.ino
+++ seamless.ino
@@ -1,6 +1,7 @@
 #include "arduinoFFT.h"
 
-/* Thank you to arduinoFFT library authors — Seamless wouldn't have been possible without their library! */
+/* Thank you to arduinoFFT library authors and Chris Parker — 
+Seamless wouldn't have been possible without their work! */
 
 arduinoFFT FFT = arduinoFFT();
 
@@ -20,70 +21,70 @@ double vImag[samples];
 pinMode(A0, INPUT);     // Microphone input
 pinMode(A1, OUTPUT);      // Output low f
 pinMode(A2, OUTPUT);      // Output high f
+pinMode(A3, OUTPUT);      // Output mid f
 
-void setup()
-{
+
+void setup() {
   Serial.begin(115200);
   while(!Serial);
   Serial.println("Ready");
 }
 
 
-
-
 void loop() {
 
   // Build raw data
   
-  double cycles = (((samples-1) * signalFrequency) / samplingFrequency); //Number of signal cycles that the sampling will read
+  double cycles = (((samples-1) * signalFrequency) / samplingFrequency); 
   for (uint16_t i = 0; i < samples; i++)
   {
-    vReal[i] = int8_t((amplitude * (sin((i * (twoPi * cycles)) / samples))) / 2.0);     // Build data with positive and negative values
-    vImag[i] = 0.0;       // Imaginary part must be zeroed in case of looping to avoid wrong calculations and overflows
+    vReal[i] = int8_t((amplitude * (sin((i * (twoPi * cycles)) / samples))) / 2.0);    
+    vImag[i] = 0.0;      
   }
   
-  // Print results
+  // FFT 
   
   Serial.println("Data:");
   PrintVector(vReal, samples, SCL_TIME);
   FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);        // Weigh data 
   Serial.println("Weighed data:");
   PrintVector(vReal, samples, SCL_TIME);
+  
   FFT.Compute(vReal, vImag, samples, FFT_FORWARD);      // Compute FFT 
   Serial.println("Computed Real values:");
   PrintVector(vReal, samples, SCL_INDEX);
   Serial.println("Computed Imaginary values:");
   PrintVector(vImag, samples, SCL_INDEX);
+  
   FFT.ComplexToMagnitude(vReal, vImag, samples);      // Compute magnitudes 
   Serial.println("Computed magnitudes:");
   PrintVector(vReal, (samples >> 1), SCL_FREQUENCY);
+  
   double x = FFT.MajorPeak(vReal, samples, samplingFrequency);
   Serial.println(x, 6);
+  
   while(1); /* Run Once */
-  // delay(2000);       // Repeat 
+  delay(30);  
 
+  //  Output peak f to circuit
 
-  l = map(a, 0, 1023, 0, 255);
-  h = map(a, 0, 1023, 0, 255);
+  f = map(x, 0, 1023, 0, 255);
 
-
-  analogWrite(A1, l);
-  analogWrite(A2, h);
-
+  if (f<300) {
+    analogWrite(A1, f);
+  } else if (f > 700) {
+    analogWrite(A2, f);
+  } else {
+    analogWrite(A3, f);
+  }
+  
+  
 }
 
 
-
-
-
-
-
-void PrintVector(double *vData, uint16_t bufferSize, uint8_t scaleType)
-{
-  for (uint16_t i = 0; i < bufferSize; i++)
-  {
+void PrintVector(double *vData, uint16_t bufferSize, uint8_t scaleType) {
+  for (uint16_t i = 0; i < bufferSize; i++) {
     double abscissa;
-    /* Print abscissa value */
     switch (scaleType)
     {
       case SCL_INDEX:
